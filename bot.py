764672075dc04112748928186118d8c41c4a86ea
Baiqing Lyu <baiqinglyu@gmail.com>
2020-01-01 17:42:57 -0500

    Cleaned up some things

--- bot.py
+++ bot.py
@@ -1,23 +1,23 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
-import os
 import cv2
 import numpy as np
-import urllib.request
 import discord
 import requests
 
+device = torch.device("cuda:0")
 
 class Net(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(1, 32, 16)
+        self.conv1 = nn.Conv2d(1, 32, 16) # Input 1, 32 features, 5 kernels to find features
         self.conv2 = nn.Conv2d(32, 64, 16)
         self.conv3 = nn.Conv2d(64, 128, 16)
-
-        x = torch.randn(256,256).view(-1,1,256,256)
+        # Convolutional layers needs to go to linear layers eventually
+        
+        # We need to pass some fake data to figure out the convnet output neurons
+        x = torch.randn(256,256).view(-1,1,256,256) # Positive 1 is related to the first 1, -1 means whatever feature set amount
         self._to_linear = None
         
         self.convs(x) # Sets self._to_linear
@@ -30,7 +30,7 @@ class Net(nn.Module):
     
     # Calculate output size of the last convolutional layer
     def convs(self, x):
-        x = F.max_pool2d(F.relu(self.conv1(x)), (3,3))
+        x = F.max_pool2d(F.relu(self.conv1(x)), (3,3)) # Shape of pooling will be 3 by 3
         x = F.max_pool2d(F.relu(self.conv2(x)), (3,3))
         x = F.max_pool2d(F.relu(self.conv3(x)), (3,3))
         
@@ -46,16 +46,11 @@ class Net(nn.Module):
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return F.softmax(x, dim = 1) # Dimension is 1 due to batches
-        
 
-
-
-device = torch.device("cuda:0")
 model = torch.load("model_final_256_256").to(device)
 
-
 def curseDetermine(link):
-    resp = requests.get(link, stream=True, headers={'User-agent': 'Mozilla/5.0'}).raw
+    resp = requests.get(link.strip(), stream=True, headers={'User-agent': 'Mozilla/5.0'}).raw
     img = np.asarray(bytearray(resp.read()), dtype="uint8")
     img = cv2.imdecode(img, cv2.IMREAD_GRAYSCALE)
     img = cv2.resize(img, (256, 256))
@@ -65,13 +60,13 @@ def curseDetermine(link):
     x = torch.Tensor([i[0] for i in target])
 
     output = model(x[0].view(-1,1,256,256).to(device))[0]
-    blessed = float(output[1])
-    cursed = float(output[0])
+    blessed = float(output[1]) * 100
+    cursed = float(output[0]) * 100
 
 
-    return ("I think this image is " + str(cursed*100) + "% cursed and " + str(blessed*100) + "% blessed")
+    return ("I am **" + np.format_float_positional(cursed, trim='-') + "%** confident that this image is **cursed** and **" + np.format_float_positional(blessed, trim='-') + "%** confident that this image is **blessed**")
 
-class MyClient(discord.Client):
+class curseNet(discord.Client):
     async def on_ready(self):
         print('Logged in as')
         print(self.user.name)
@@ -83,10 +78,10 @@ class MyClient(discord.Client):
         if message.author.id == self.user.id:
             return
 
-        if message.content.startswith('!cursebot'):
+        if message.content.startswith('!howcursed'):
             try:
-                if len(message.content) > 9:
-                    result = curseDetermine(message.content[9:])
+                if len(message.content) > 10:
+                    result = curseDetermine(message.content[10:])
                 else:
                     result = curseDetermine(message.attachments[0].url)
                 await message.channel.send(result.format(message))
@@ -95,5 +90,5 @@ class MyClient(discord.Client):
 
 
 
-client = MyClient()
-client.run("") # Input your Discord API Key here
+client = curseNet()
+client.run("[ENTER API]")
